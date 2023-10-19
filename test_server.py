c9cfc471adfba103b4fbdee8437cc3ce6d89c157
decamarkers <ecyhtots@gmail.com>
2023-10-19 10:35:02 +0800

    update requirements

--- test_server.py
+++ test_server.py
@@ -61,32 +61,36 @@ def process_question():
 #   result = r.recognize_google(audio_data=audio_data, language="en-US", show_all=True)
 #   return jsonify({"text": result})
 
-# import whisper
+import whisper
 
-# @app.route("/convertSpeechToText/<string:recordingFilepath>", methods=["GET"])
-# def convertSpeechToText(recordingFilepath):
-#   # pip install -U openai-whisper
-#   # May need FFMPEG? https://www.geeksforgeeks.org/how-to-install-ffmpeg-on-windows/
-#   print(recordingFilepath)
-#   # Set model filepath
-#   # Default code to download: model = whisper.load_model("base")
-#   # After downloading, model will be stored in C:\Users\<username>\.cache\whisper\<model>
-#   model = whisper.load_model("base.pt")
-#   # load audio and pad/trim it to fit 30 seconds
-#   # audio should be mp3/wav? Need to check, m4a seems to work
-#   audio = whisper.load_audio("flu symptoms.m4a") # Replace with recordingFilepath
-#   audio = whisper.pad_or_trim(audio)
-#   # make log-Mel spectrogram and move to the same device as the model
-#   mel = whisper.log_mel_spectrogram(audio).to(model.device)
-#   # detect the spoken language
-#   _, probs = model.detect_language(mel)
-#   print(f"Detected language: {max(probs, key=probs.get)}")
-#   # decode the audio
-#   options = whisper.DecodingOptions(fp16 = False)
-#   result = whisper.decode(model, mel, options)
-#   # print the recognized text
-#   print(result.text)
-#   return(result.text)
+@app.route("/convertSpeechToText/<string:recordingFilepath>", methods=["GET"])
+def convertSpeechToText(recordingFilepath):
+  # pip install -U openai-whisper
+  # Install FFMPEG https://www.geeksforgeeks.org/how-to-install-ffmpeg-on-windows/
+
+  # Set model filepath
+  # Default code to download: model = whisper.load_model("base")
+  # After downloading, model will be stored in C:\Users\<username>\.cache\whisper\<model>
+  model = whisper.load_model("base")
+
+  # audio should be mp3/wav? Need to check, m4a seems to work
+  audio = whisper.load_audio("flu symptoms.m4a") # Replace with recordingFilepath
+
+  # load audio and pad/trim it to fit 30 seconds
+  audio = whisper.pad_or_trim(audio)
+
+  # make log-Mel spectrogram and move to the same device as the model
+  mel = whisper.log_mel_spectrogram(audio).to(model.device)
+  
+  # detect the spoken language
+  _, probs = model.detect_language(mel)
+  print(f"Detected language: {max(probs, key=probs.get)}")
+  # decode the audio
+  options = whisper.DecodingOptions(fp16 = False)
+  result = whisper.decode(model, mel, options)
+  # print the recognized text
+  print(result.text)
+  return(result.text)
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=42069, debug=True)
